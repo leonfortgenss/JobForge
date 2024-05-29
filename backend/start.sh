c243ad07b59d8c6fb067c460c38c07d35e04f0fd
Leon <leonfortgens@hotmail.se>
2024-05-29 14:23:50 +0200

    Updated nox and procfile

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-python -m venv venv
-source venv/bin/activate
-
-pip install -r requirements.txt
-
 python ./src/jobsearch.py
 python ./src/data_explore.py
 python ./src/create_db.py
 python ./src/query_and_visualize.py
-
-python manage.py collectstatic --noinput
-
-python manage.py migrate
 
 gunicorn backend.wsgi:application --log-file -
